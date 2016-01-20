2e761e09b8c2e422a4cd8bc2507eee6b9e0cb85a
steneu <freifunk@egfd.de>
2016-01-21 00:36:44 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,3 +1,9 @@
+##	gluon site.mk makefile
+
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+
 GLUON_SITE_PACKAGES := \
     gluon-mesh-batman-adv-15 \
     gluon-alfred \
@@ -15,21 +21,38 @@ GLUON_SITE_PACKAGES := \
     gluon-luci-admin \
     gluon-luci-autoupdater \
     gluon-luci-portconfig \
-    gluon-luci-private-wifi \
+    gluon-luci-wifi-config \
     gluon-next-node \
     gluon-mesh-vpn-fastd \
     gluon-radvd \
     gluon-setup-mode \
     gluon-status-page \
-    gluon-authorized-keys \
+    gluon-ssid-changer \
     iwinfo \
     iptables \
     haveged \
     
-DEFAULT_GLUON_RELEASE := 2015.2-beta-0.7.0-$(shell date '+%Y%m%d')
-# Allow overriding the release number from the command line
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
 
+DEFAULT_GLUON_RELEASE := 0.7.1~rdv-beta$(shell date '+%Y%m%d')
+
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+# Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
+
+# Languages to include
 GLUON_LANGS ?= en de
-GLUON_BRANCH ?= beta
