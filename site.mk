52101eec7fb8f4fbd90a6d9392ed69afe8c965dc
steneu <freifunk@egfd.de>
2016-05-18 14:48:28 +0200

    Versionsnummer auf 0.7.3 gesetzt

--- site.mk
+++ site.mk
@@ -39,7 +39,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.7.2-rdv-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.3-rdv-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
