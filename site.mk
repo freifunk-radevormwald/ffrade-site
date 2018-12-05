d96d2132feb70b90ccfb613ad62b619433f72aa0
akoyaxd <koyax@protonmail.com>
2018-12-05 20:09:45 +0100

    Bumb Version

--- site.mk
+++ site.mk
@@ -5,7 +5,7 @@
 #               gluon relies on
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
-DEFAULT_GLUON_RELEASE := v2018.1.3-0
+DEFAULT_GLUON_RELEASE := v2018.1.3-2
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
