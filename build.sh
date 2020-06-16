81d5ebec0ff6c0dd5224d55e4d9bb9802fb44d9f
steneu <freifunk@egfd.de>
2020-06-17 01:06:55 +0200

    erste anpassung

--- build.sh
+++ build.sh
@@ -5,19 +5,21 @@ git pull
 popd
 make update
 
-# apply eulenfunk respondd patch https://github.com/eulenfunk/firmware/raw/v2019.1.x/patches/fix-respondd-rsk.patch
+## apply eulenfunk respondd patch https://github.com/eulenfunk/firmware/raw/v2019.1.x/patches/fix-respondd-rsk.patch
 patchfile="patches/fix-respondd-rsk.patch"
 if ! patch -R -p1 -s -f --ignore-whitespace --dry-run < $patchfile; then
   patch -p1 --ignore-whitespace < $patchfile
 fi
 grep 'ff02:'  package/gluon-respondd/files/etc/init.d/gluon-respondd
 
-# build firmware images
-branch="experimental"
+## build firmware images
+branch="stable"             # Möglichkeiten sind: stable beta experimental
+release="2020.1.-rdv-2.2"
+
 cores=$(($(nproc) + 1))
 
-# make -j$cores GLUON_TARGET=ar71xx-generic GLUON_BRANCH=$branch
-make -j$cores GLUON_TARGET=ar71xx-tiny GLUON_BRANCH=$branch
+make -j$cores GLUON_TARGET=ar71xx-generic GLUON_BRANCH=$branch DEFAULT_GLUON_RELEASE=$release
+make -j$cores GLUON_TARGET=ar71xx-tiny GLUON_BRANCH=$branch DEFAULT_GLUON_RELEASE=$release
 # make -j$cores GLUON_TARGET=ar71xx-nand GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=brcm2708-bcm2708 GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=brcm2708-bcm2709 GLUON_BRANCH=$branch
@@ -28,7 +30,7 @@ make -j$cores GLUON_TARGET=ar71xx-tiny GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=x86-generic GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=x86-geode GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=x86-64 GLUON_BRANCH=$branch
-make -j$cores GLUON_TARGET=ipq40xx GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=ipq40xx GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=ramips-mt7620 GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=ramips-mt76x8 GLUON_BRANCH=$branch
 # make -j$cores GLUON_TARGET=ramips-rt305x GLUON_BRANCH=$branch
