99a497481f9c8829b9d21b3b8ee186a429ae885f
steneu <freifunk@egfd.de>
2020-06-16 23:06:36 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pushd site
+git pull
+popd
+make update
+
+# apply eulenfunk respondd patch https://github.com/eulenfunk/firmware/raw/v2019.1.x/patches/fix-respondd-rsk.patch
+patchfile="patches/fix-respondd-rsk.patch"
+if ! patch -R -p1 -s -f --ignore-whitespace --dry-run < $patchfile; then
+  patch -p1 --ignore-whitespace < $patchfile
+fi
+grep 'ff02:'  package/gluon-respondd/files/etc/init.d/gluon-respondd
+
+# build firmware images
+branch="experimental"
+cores=$(($(nproc) + 1))
+
+# make -j$cores GLUON_TARGET=ar71xx-generic GLUON_BRANCH=$branch
+make -j$cores GLUON_TARGET=ar71xx-tiny GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=ar71xx-nand GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=brcm2708-bcm2708 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=brcm2708-bcm2709 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=mpc85xx-generic GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=mpc85xx-p1020 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=ramips-mt7621 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=sunxi-cortexa7 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=x86-generic GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=x86-geode GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=x86-64 GLUON_BRANCH=$branch
+make -j$cores GLUON_TARGET=ipq40xx GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=ramips-mt7620 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=ramips-mt76x8 GLUON_BRANCH=$branch
+# make -j$cores GLUON_TARGET=ramips-rt305x GLUON_BRANCH=$branch
